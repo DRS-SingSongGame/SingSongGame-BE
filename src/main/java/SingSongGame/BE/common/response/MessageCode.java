8aeef2b78d80fab6bb4fab6d873af07f55c53381
capDoYeonLee <ghtyu21@naver.com>
2025-06-27 11:40:49 +0900

    feat: response 형식 통일 및 swagger 문서 도입 #6

---
+++ src/main/java/SingSongGame/BE/common/response/MessageCode.java
@@ -0,0 +1,27 @@
+package SingSongGame.BE.common.response;
+
+public enum MessageCode {
+    CREATE("200", "생성 성공"),
+    GET("200", "조회 성공"),
+    UPDATE("200", "수정 성공"),
+    DELETE("200", "삭제 성공"),
+    LOGIN("200", "로그인 성공"),
+    LOGOUT("200", "로그아웃 성공"),
+    REISSUE("200", "ACCESS TOKEN 재발급 성공");
+
+    private final String code;
+    private final String message;
+
+    MessageCode(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
