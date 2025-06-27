059499c6f24701b9ea2a5a37dfe486e72b41febc
capDoYeonLee <ghtyu21@naver.com>
2025-06-27 21:40:31 +0900

    feat: 게임 방 생성 기능 #11

---
+++ src/main/java/SingSongGame/BE/room/application/RoomService.java
@@ -0,0 +1,38 @@
+package SingSongGame.BE.room.application;
+
+import SingSongGame.BE.room.application.dto.request.CreateRoomRequest;
+import SingSongGame.BE.room.persistence.GameStatus;
+import SingSongGame.BE.room.persistence.Room;
+import SingSongGame.BE.room.persistence.RoomRepository;
+import SingSongGame.BE.auth.persistence.AuthRepository;
+import SingSongGame.BE.auth.persistence.User;
+import SingSongGame.BE.room.persistence.RoomType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class RoomService {
+
+    private final RoomRepository roomRepository;
+    private final AuthRepository authRepository;
+
+    @Transactional
+    public void createRoom(CreateRoomRequest request) {
+        //User host = authRepository.findById(request.getHostId())
+        //        .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+        Room room = Room.builder()
+                .name(request.getName())
+                .room(request.getRoomType())
+                .isPrivate(request.getIsPrivate())
+                .password(request.getRoomPassword())
+                .maxPlayer(request.getMaxPlayer())
+                .gameStatus(GameStatus.valueOf("WAITING"))
+                //.host(host) // 소셜 로그인 구현 후 수정 필요.
+                .build();
+        roomRepository.save(room);
+    }
+}
