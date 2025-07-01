3182bf78981d7105b363a2280e1d51f9eb79dd32
capDoYeonLee <ghtyu21@naver.com>
2025-07-02 05:30:03 +0900

    feat: 특정 게임 방 내부 실시간 채팅 기능 #45

---
+++ src/main/java/SingSongGame/BE/chat/dto/RoomChatRequest.java
@@ -0,0 +1,13 @@
+package SingSongGame.BE.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoomChatRequest {
+    private Long roomId;
+    private String message;
+} 
